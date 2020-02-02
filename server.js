3f39fb30556a36f6bd19d399dad5c8056525299a
Ackyla Ferreira Batista <ackylafb@gmail.com>
2020-02-01 22:45:52 -0300

    server and path to frontend ready

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
+
 const server = express()
 
-server.get('/', function(req, res){
-    return res.send('Hello World')
+server.use(express.static('public'))
+
+server.set("view engine", "html")
+
+nunjucks.configure("views", {
+    express:server
+})
+
+server.get("/", function(req, res){
+    return res.render("index")
+})
+
+server.get("/sobre", function(req, res){
+    return res.render("sobre")
+})
+
+server.get("/receitas", function(req, res){
+    return res.render("receitas")
 })
 
 server.listen(5000, function(){
