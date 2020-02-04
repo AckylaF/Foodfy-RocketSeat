31c9d0d111ed49284cafe690642f5f5820b4fdc8
Ackyla Ferreira Batista <ackylafb@gmail.com>
2020-02-04 11:48:42 -0300

    next step: set css and js

--- server.js
+++ server.js
@@ -24,6 +24,21 @@ server.get("/receitas", function(req, res){
     return res.render("receitas", {items: cards})
 })
 
+server.get("/recipes", function(req, res){
+    const id = req.query.id
+    const card = cards.find(function(card){
+        if(card.id == id){
+            return true
+        }
+    })
+
+    if(!card){
+        return res.send('Recipe not found')
+    }
+
+    return res.render("recipes", {item: card})
+})
+
 server.listen(5000, function(){
     console.log('Server is running');    
 })
